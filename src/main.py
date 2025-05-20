5b88892476d4878e606f82950d9874950e3b2b8d
Tom-Petty98 <tompetty527@yahoo.co.uk>
2025-05-20 15:56:29 +0100

    Enhanced md generation from multiple files

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import os
 import shutil
-from markdown_to_html import markdown_to_html_node, extract_h1
+from markdown_blocks import markdown_to_html_node, extract_h1
 
 def main():
     copy_files_to_public()
-    generate_page("./content/index.md", "./content/template.html", "./public/index.html")
+    generate_pages("./content", "./template.html", "public")
 
 
 def copy_files_to_public():
@@ -27,7 +27,29 @@ def copy_files(current_directory="./static"):
         else:
             os.mkdir(file_path.replace("static", "public"))
             copy_files(file_path)
+            
 
+def generate_pages(dir_path_content, template_path, dest_dir_path):
+    file_paths = list_files_recursive(dir_path_content)
+
+    for file_path in file_paths:
+        dest_path = file_path.replace("content", dest_dir_path)
+        dest_path = dest_path.replace(".md", ".html")
+        generate_page(file_path, template_path, dest_path)
+
+
+def list_files_recursive(source_path):
+    file_paths = []
+
+    dir_content = os.listdir(source_path)
+    for item in dir_content:
+        file_path = source_path + "/" + item
+        if os.path.isfile(file_path):
+            file_paths.append(file_path)
+        else:
+            file_paths.extend(list_files_recursive(file_path))
+    
+    return file_paths
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -46,8 +68,10 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", inner_html)
 
-    # dirname = os.path.dirname(dest_path)
-    # os.makedirs(dirname)
+    dest_dir_path = os.path.dirname(dest_path)
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
+
     html_file = open(dest_path, 'w')
     html_file.write(template)
     html_file.close()
